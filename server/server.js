be4d41c53ed0fd3c26458d4a818282fe0d153ef6
Austin Burgard <awburgard@gmail.com>
2019-05-04 11:21:57 -0500

    linked in all files by fixing spelling error

--- server/server.js
+++ server/server.js
@@ -5,13 +5,13 @@ const PORT = process.env.PORT || 5000;
 const toDoRouter = require('./routes/todoapp.router');
 
 // Acquires body-parser and static files
-app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static('server/public'));
 
 // Defines the name of routes and acquires usage of the router file
-app.use('/toDo',toDoRouter);
+app.use('/toDo', toDoRouter);
 
 // Turns on the Port
-app.listen(PORT, ()=>{
-    console.log('Listening pn Port: ', PORT)
+app.listen(PORT, () => {
+    console.log('Listening on port: ', PORT);
 });
