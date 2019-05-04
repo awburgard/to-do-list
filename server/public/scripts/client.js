7dfdfb4d4e489288b7ae45071fb2eacd2e736fc1
Austin Burgard <awburgard@gmail.com>
2019-05-04 11:38:13 -0500

    added input and button and assigned event listener

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -2,4 +2,9 @@ $(document).ready(onReady);
 
 function onReady(){
     console.log('Im ready!');
+    $('.js-btn-task').on('click', postTask);
 };
+
+function postTask(){
+    console.log('I work!');
+}
