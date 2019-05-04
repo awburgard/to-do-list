3f9e115ed4e4d2eabe939a5667fecdc913eee60a
Austin Burgard <awburgard@gmail.com>
2019-05-04 10:39:47 -0500

    set up intitial router file

---
+++ server/routes/todoapp.router.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const toDoRouter = express.Router();
+
+const pool = require('../modules/pool');
+
+//GET
+
+//POST
+
+//DELETE
+
+//PUT
+
+module.exports = toDoRouter;
